2b42f1ae5914084e7738b274aff03d1af3125ac3
bpgabrielli <bpgabrielli@gmail.com>
2015-05-07 21:50:59 -0400

    Colorize new tags

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,8 +6,9 @@ class Recipe < ActiveRecord::Base
   # has_many :steps
 
   def all_tags=(names)
+    tag_palette = ["#979797", "#EA7635", "#0C8243", "#5E82C1", "#6F459B", "#D14D52", "#28B893", "#F2A82F", "#42B149", "#2E7FC2", "#9F6F4C", "#4AB9D2", "#D44393", "#6F64AD", "#BBBBBB", "#76C58C"]
     self.tags = names.split(",").map do |title|
-      Tag.where(title: title.strip, user_id: self.user_id).first_or_create!
+      Tag.where(title: title.strip.downcase, user_id: self.user_id, color: tag_palette.sample).first_or_create!
     end
   end
 
